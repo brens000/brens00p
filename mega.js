a411408556f9546bf4c17dffb0bc461692385f9d
brens000 <jemmalouis7@gmail.com>
2025-07-29 13:42:05 -0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'jemmalouis7gmail.com',
-    password: '1234au()00gu',
+    email: 'alixexadron7gmail.com',
+    password: '@Alix54321-M',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
